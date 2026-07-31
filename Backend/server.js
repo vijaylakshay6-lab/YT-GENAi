4016c53f57f69ce33c076af7c3a3e61a83dd1614
vijaylakshay6-lab <vijaylakshay6@gmail.com>
2026-07-31 21:42:32 +0530

    first commit

---
+++ Backend/server.js
@@ -0,0 +1,10 @@
+require("dotenv").config()
+const app = require("./src/app")
+const connectDB = require("./src/config/db")
+
+connectDB()
+
+
+app.listen(3000, () =>{
+    console.log("Server is ready on PORT 3000")
+})
